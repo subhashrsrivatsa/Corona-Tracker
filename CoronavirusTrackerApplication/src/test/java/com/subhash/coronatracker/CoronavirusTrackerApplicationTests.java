8b31f1cac6745b064926e290e286fc9330c8947f
subhashrsrivatsa <subhashrsrivatsa.paris@atlassian.com>
2020-12-01 23:25:43 +0100

    initial commit

---
+++ CoronavirusTrackerApplication/src/test/java/com/subhash/coronatracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhash.coronatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
